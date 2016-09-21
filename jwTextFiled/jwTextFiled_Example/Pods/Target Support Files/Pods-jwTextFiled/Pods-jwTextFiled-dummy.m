0356fbcdcf440b1feca3ae516aaee741f3ce890b
Mac_NJW <juziboluomi@163.com>
2016-09-21 09:16:35 +0800

    删除

--- jwTextFiled/jwTextFiled_Example/Pods/Target Support Files/Pods-jwTextFiled/Pods-jwTextFiled-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_jwTextFiled : NSObject
-@end
-@implementation PodsDummy_Pods_jwTextFiled
-@end
--- jwTextFiled/jwTextFiled_Example/Pods/Target Support Files/Pods-jwTextFiledUITests/Pods-jwTextFiledUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_jwTextFiledUITests : NSObject
-@end
-@implementation PodsDummy_Pods_jwTextFiledUITests
-@end
